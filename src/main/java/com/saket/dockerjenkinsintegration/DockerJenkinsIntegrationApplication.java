6950c48a5e365c28dc16a822c6ab0dbbdca3f85b
Saket Sinha <saket.sinha@active.ai>
2020-05-26 22:51:06 +0530

    Message second commit

--- src/main/java/com/saket/dockerjenkinsintegration/DockerJenkinsIntegrationApplication.java
+++ src/main/java/com/saket/dockerjenkinsintegration/DockerJenkinsIntegrationApplication.java
@@ -1,11 +1,14 @@
 package com.saket.dockerjenkinsintegration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DockerJenkinsIntegrationApplication {
 
+	Logger logger = LoggerFactory.getLogger("Integrating Jenkins with Github");
 	public static void main(String[] args) {
 		SpringApplication.run(DockerJenkinsIntegrationApplication.class, args);
 	}
