6475447199c2cb82e3564de26ab6a1299707536b
Saket Sinha <saket.sinha@active.ai>
2020-05-26 22:44:47 +0530

    First Commit

---
+++ src/main/java/com/saket/dockerjenkinsintegration/DockerJenkinsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.saket.dockerjenkinsintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saket/dockerjenkinsintegration/DockerJenkinsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saket.dockerjenkinsintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
